3827461b34f961ad7a5c511679da5e19c48391c3
nedgip <christopher.pigden@gmail.com>
2020-04-27 22:10:46 +1000

    Fix issues new book page

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,12 @@
 const rootStyles = window.getComputedStyle(document.documentElement);
+
 if (
   rootStyles.getPropertyValue("--book-cover-width-large") != null &&
-  rootStyles.getPropertyValue("--book-cover-width-large") != ""
+  rootStyles.getPropertyValue("--book-cover-width-large") !== ""
 ) {
   ready();
 } else {
-  document
-    .getElementByIdElementByID("main-css")
-    .addEventListener("load", ready);
+  document.getElementById("main-css").addEventListener("load", ready);
 }
 
 function ready() {
@@ -18,16 +17,17 @@ function ready() {
     rootStyles.getPropertyValue("--book-cover-aspect-ratio")
   );
   const coverHeight = coverWidth / coverAspectRatio;
-
   FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode
   );
+
   FilePond.setOptions({
     stylePanelAspectRatio: 1 / coverAspectRatio,
     imageResizeTargetWidth: coverWidth,
     imageResizeTargetHeight: coverHeight,
   });
+
   FilePond.parse(document.body);
 }
